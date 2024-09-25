919de48a718cf2f2da8b3c8ac5dd9fdf3fbe2f7e
Serhat Barış AYDİN <serhatbarisaydin@gmail.com>
2024-09-25 17:30:44 +0300

    json-server and axios installation
    kayıt içeren bilgiler users.json dosyasına aktarılmak üzere yani branch açıldı.

--- src/pages/Settings/SettingsPage.vue
+++ src/pages/Settings/SettingsPage.vue
@@ -8,15 +8,33 @@
     <div>
       <UserInfo />
     </div>
-  </div>
+
+      <div>
+        <p style="color: black;" v-for="user in users" :key="user.id" > {{ user.name }} </p>
+      </div>
+
+
+</div>
 </template>
 
 <script>
 import BreadCrumbComponent from "../../components/BreadCrumbComponent.vue";
 import LogoComponent from "../../components/LogoComponent.vue";
 import UserInfo from "../../components/UserInfo.vue";
+import userData from "../../users.json"
+
+
+
+
 
 export default {
+
+
+  data(){
+  return{
+    users: userData,
+  }
+},
   components: {
     UserInfo,
     BreadCrumbComponent,
